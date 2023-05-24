db748149a21dc7c78a7b6705d7b709a9fdd4a447
Rhythm Jain <rhythmbytes@gmail.com>
2023-05-24 14:22:13 +0200

    cleanup and add users component

--- apps/explore-users/src/app/app.routes.ts
+++ apps/explore-users/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { UsersComponent } from './users/users.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '**',
+        component: UsersComponent
+    }
+];
---
+++ apps/explore-users/src/app/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'rabobank-users',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+})
+export class UsersComponent {}
